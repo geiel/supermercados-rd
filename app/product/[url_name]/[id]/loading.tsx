57fa105891bf586f7dc71996230d79533398c641
geiel <geielpeguero@gmail.com>
2025-10-31 19:03:43 -0400

    improve loading product

--- app/product/[url_name]/[id]/loading.tsx
+++ app/product/[url_name]/[id]/loading.tsx
@@ -2,14 +2,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-10 py-4 px-4 md:px-10">
+    <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-10 py-4 px-4 md:px-10 space-y-2">
         <div className="flex flex-col gap-2">
             <Skeleton className="h-5 w-[250px]" />
             <Skeleton className="h-3 w-[300px]" />
             <Skeleton className="h-4 w-[50px]" />
             <Skeleton className="h-[290px] w-full md:h-[500px] md:w-full rounded-xl" />
         </div>
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-5">
             <Skeleton className="h-5 w-[250px]" />
             <div className="flex flex-col gap-2">
                 <Skeleton className="h-10 w-full" />
