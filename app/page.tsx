70c374634dc473e1143ff185d85eafe6021c5956
geiel <geielpeguero@gmail.com>
2026-01-28 12:54:07 -0400

    fux

--- app/page.tsx
+++ app/page.tsx
@@ -76,6 +76,7 @@ export default function Home() {
                       sizes="100vw"
                       className="w-[70px] h-auto"
                       alt={`Logo ${shop.name}`}
+                      unoptimized
                     />
                   </Link>
                 </Button>
